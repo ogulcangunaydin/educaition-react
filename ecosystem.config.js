7d9a6f172cc95081ce9a936204e0a87869ef9fcc
Oğulcan Günaydın <ogulcangunaydin@outlook.com>
2024-07-24 18:30:53 +0300

    fix name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: 'ec2-user',
       host: 'ec2-54-173-57-250.compute-1.amazonaws.com',
       key: '~/.ssh/educaition-key-pair.pem',
-      ref: 'origin/main',
+      ref: 'origin/master',
       repo: 'git@github.com:ogulcangunaydin/educaition-react.git',
       path: '/home/ec2-user/educaition-react',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
