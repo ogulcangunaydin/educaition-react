4c8e66c920d88eab6023fd60dade2016b5c0abb6
Oğulcan Günaydın <ogulcangunaydin@outlook.com>
2025-12-04 00:10:20 +0300

    Add logo also to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
 // Header.js
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 function Header({ title, children }) {
   return (
-    <AppBar position="fixed" style={{ backgroundColor: 'blue' }}>
+    <AppBar position="fixed" sx={{ bgcolor: "primary.light" }}>
       <Toolbar>
+        <Box
+          component="img"
+          src="/halic_universitesi_logo.svg"
+          alt="Haliç Üniversitesi"
+          sx={{
+            height: 40,
+            mr: 2,
+          }}
+        />
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           {title}
         </Typography>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          {children}
-        </Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>{children}</Box>
       </Toolbar>
     </AppBar>
   );
